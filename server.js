60f8f3b5a7444da8c5afd12500e838260044a507
adarshjain9628@gmail.com <aduilu2k15>
2017-05-11 00:12:54 +0530

    updated mondo location

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ mongoose = require('mongoose'),
   bodyParser = require('body-parser');
   
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/Tododb'); 
+mongoose.connect('mongodb://buvb89k3u6yr7w5-mongodb.services.clever-cloud.com:27017/buvb89k3u6yr7w5'); 
 
 
 app.use(bodyParser.urlencoded({ extended: true }));
